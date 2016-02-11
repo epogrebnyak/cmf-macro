235d49093c8460e60c6dda762aafe9656d06a028
Alexander Pisanov <pisanov.alexander@gmail.com>
2016-02-11 16:22:29 +0300

    Update access_fred.r

--- access_fred.r
+++ access_fred.r
@@ -1,113 +1,90 @@
-###
-### Интерфейс доступа к базе данных FRED - код на R
-### 11.02.2016
-### Автор: Александр Писанов
-### 
-### Использование: 
-###      
-### GDPC1 <- fred.data.retriever("GDPC1","1947-01-01","2016-10-01")
-###
-###
+## Version: 2.1; release date: 12.02.2016; developer: Alexander Pisanov (any questions - please, email on pisanov.alexander@gmail.com).
+## Description: 2 functions: fred.data.description(id), fred.data.value(id,start,end,csv).
+## Parameters: id - series id from FRED website; start; end; csv - 1 if you want a .csv file to be written on your disk, 0 if not.
+## Syntax: x1 <- fred.data.description("GDPC1"); x2 <- fred.data.value("GDPC1","1947-01-01","2016-01-01",1).
+## Output: x1 - character vector, x2 - numeric vector, .csv file with dates and values delimited.
+## WARNING: correct work with submonthly frequencies is not guaranteed.
 
 ####################
-# FRED INTERFACE:
+#FRED INTERFACE:####
 ####################
 
-# Endpoint and file format setup (in case they change over time):
+# WORKING FOLDER, FRED ENDPOINT AND FILE FORMAT SETUP:
 
+library(zoo) # Zoo library is required.
+
+my.working.folder <- setwd("C:/Users/Alexander Pisanov/Desktop")
 fred.data.endpoint <- "https://research.stlouisfed.org/fred2/data/"
 fred.data.format <- ".txt"
 
-# Retrieval function (currently with no frequency selection option):
+# DATA DESCRIPTION FUNCTION:
 
-fred.data.retriever <- function(data.id,data.start,data.end)
+fred.data.description <- function(id)
 {
-  fred.data.file <- paste0(fred.data.endpoint,data.id,fred.data.format)
+  err <- 0
+  
+  fred.data.file <- paste0(fred.data.endpoint,id,fred.data.format)
   data <- readLines(fred.data.file)
+  data.id <- unlist(strsplit(gsub(" ","",data[2],fixed=TRUE),split=":"))[2]
   
-  # todo: сложное выражение лучше в отельную функцию + непонятно что такое max.frequency
-  max.frequency <- unlist(strsplit(gsub(" ","",data[6],fixed=TRUE),split=":"))[2]
+  if (!(data.id==id)) err <- err+1 else err
+  if (!(data.id==id)) print("Error: 'fred.data.file' time series id doesn't match the id set by user. Please, report to the developer.")
   
-  y1 <- function(max.frequency)
-  {
-    if (max.frequency=="Monthly") {x1 <- 1/12}
-    else if (max.frequency=="Quarterly") {x1 <- 1/4}
-    else if (max.frequency=="Semiannual") {x1 <- 1/2}
-    else if (max.frequency=="Annual") {x1 <- 1}
-    {return(x1)}
-  }
+  parse <- gsub(" ","",data,fixed=TRUE)
+  parse.index <- match("DATEVALUE",parse)-1
+  data.parse <- data[1:parse.index]
+  
+  if (err>0) output <-0 else output <- data.parse
+  
+  {return(output)}
+}
 
-# комментарий непонятный
-# NOTES: data.frequency <- max.frequency should be deleted.  
-    
-  data.frequency <- max.frequency
-  y2 <- function(data.frequency)
-  {  
-    if (data.frequency=="Monthly") {x2 <- 1/12}
-    else if (data.frequency=="Quarterly") {x2 <- 1/4}
-    else if (data.frequency=="Semiannual") {x2 <- 1/2}
-    else if (data.frequency=="Annual") {x2 <- 1}
-    {return(x2)}
-  }
+# DATA VALUES FUNCTION:
+
+fred.data.values <- function(id,start,end,csv)
+{
+  err <-0
   
-  max.frequency <- y1(max.frequency)
-  data.frequency <- y2(data.frequency)
+  # This function block gets and parses the .txt file into a numeric vector "data.parse":
   
-  # два выраженияниже - в отедльные функции 
-  min.start <- as.Date(substring(gsub(" ","",data[8],fixed=TRUE),11,20))
-  max.end <- as.Date(substring(gsub(" ","",data[8],fixed=TRUE),23,32))
+  fred.data.file <- paste0(fred.data.endpoint,id,fred.data.format)
+  data <- readLines(fred.data.file)
+  data.id <- unlist(strsplit(gsub(" ","",data[2],fixed=TRUE),split=":"))[2]
+  data.start <- as.Date(substring(gsub(" ","",data[8],fixed=TRUE),11,20))
+  data.end <- as.Date(substring(gsub(" ","",data[8],fixed=TRUE),23,32))
   
-  # тоже в отдельную функцию с комментарием
-  if (!((data.frequency>=max.frequency)&(min.start<=data.start)&(max.end>=data.end))) err <- 1 else err <-0
-  if (err>0) stop("Check date (data.start/end) and/or frequency (data.frequency) parameters. Date should be not less that the series start date; frequency should be not higher")
+  if (!(data.id==id)) err <- err+1 else err
+  if (!(data.id==id)) print("Error: 'fred.data.file' time series' id doesn't match the id set by user. Report to the developer.")
+  if (!((data.start<=start)&(data.end>=end))) err <- err+1 else err <- err
+  if (!((data.start<=start)&(data.end>=end))) print("Error: check dates (start/end) parameters. Start/end date should be in time-series' date range. Output is set to zero. You can use 'fred.data.description' function for date range info.")
   
   parse <- gsub(" ","",data,fixed=TRUE)
   parse.index <- match("DATEVALUE",parse)
-  parse.data <- data[-(1:parse.index)]
-  parse.data.vector <- substring(gsub(" ","",parse.data,fixed=TRUE),11,100)
+  parse <- data[-(1:parse.index)]
+  date.parse <- as.Date(substring(gsub(" ","",parse,fixed=TRUE),1,10))
+  data.parse <- as.numeric(substring(gsub(" ","",parse,fixed=TRUE),11,100))
   
-  y3 <- function(max.frequency)
-  {
-    if (max.frequency==1) {x3 <- as.numeric(format(as.POSIXct(min.start),"%Y"))}
-    else if (max.frequency<1) {x3 <- c(as.numeric(format(as.POSIXct(min.start),"%Y")),as.numeric(format(as.POSIXct(min.start),"%m")))}
-    {return (x3)}
-  }
+  # This function block converts "data.parse" into a "zoo" object and trims out the window of a requested width, outputs the window and writes .csv file if csv=1.
   
-  ts.data <- ts(data=parse.data.vector,start=y3(max.frequency),deltat=max.frequency)
-
-# если это просто чатсота - почему она не называется frequency? пусть сейчас будет frequency = max.frequency
-# NOTES: hereinafter, max.frequency should be changed to data.frequency to allow for frequency selection.
+  zoo.data <- zoo(data.parse,order.by=date.parse)
+  if (err>0) output <-0 else {output <- as.matrix(window(zoo.data,start=start,end=end));colnames(output) <- id}
+  if (csv>0) {write.table(zoo(output,order.by=index(output)),file=paste0(id,".csv"),row.names=TRUE,sep=";",dec=",");print(paste0(id,".csv was written to ",my.working.folder,"."))} else print(".csv was not requested.")
   
-  y4 <- function(max.frequency)
-  {
-    if (max.frequency==1) {x4 <- as.numeric(format(as.POSIXct(data.start),"%Y"))}
-    else if (max.frequency==1/4) {x4 <- c(as.numeric(format(as.POSIXct(data.start),"%Y")),as.numeric(paste0(0,substring(quarters.POSIXt(data.start),2,2))))}
-    else if (max.frequency==1/12) {x4 <- c(as.numeric(format(as.POSIXct(data.start),"%Y")),as.numeric(format(as.POSIXct(data.start),"%m")))}
-    {return(x4)}
-  }
-  
-  y5 <- function(max.frequency)
-  {
-    if (max.frequency==1) {x5 <- as.numeric(format(as.POSIXct(data.end),"%Y"))}
-    else if (max.frequency==1/4) {x5 <- c(as.numeric(format(as.POSIXct(data.end),"%Y")),as.numeric(paste0(0,substring(quarters.POSIXt(data.end),2,2))))}
-    else if (max.frequency==1/12) {x5 <- c(as.numeric(format(as.POSIXct(data.end),"%Y")),as.numeric(format(as.POSIXct(data.end),"%m")))}
-    {return(x5)}
-  }
-  
-  output <- window(ts.data,start=y4(max.frequency),end=y5(max.frequency))
-    
   {return (output)}
 }
 
-# Example:
-GDPC1 <- 0 # To return zero if the function fails.
-GDPC1 <- fred.data.retriever("GDPC1","1947-01-01","2016-10-01") # Using the function to get quarterly US real GDP data. Series start date is set as 1937-01-01.
+# EXAMPLE:
+GDPC1 <- fred.data.description("GDPC1") # Series' id is set as a parameter.
+GDPC1 # Data sescription should have been retrieved successfully.
+
+GDPC1 <- fred.data.values("GDPC1","1947-01-01","2016-01-01",0) # Series' end date is set incorrectly, .csv disabled.
 GDPC1 # Zero with an error message.
 
-GDPC1 <- 0 # To return zero if the function fails.
-GDPC1 <- fred.data.retriever("GDPC1","1947-01-01","2015-08-01") # Retrying with correct date range.
-GDPC1 # Data was retrieved successfully.
+GDPC1 <- fred.data.values("GDPC1","1947-01-01","2015-09-01",1) # Retrying with a correct date range, .csv enabled.
+GDPC1 # Data values should have been retrieved successfully.
 
 ####################
-# YOUR CODE:
+#YOUR CODE HERE:####
 ####################
+
+## TODO: submonthly frequencies.
